275e623032c682e06414fced17c6da84351e8194
Paul Hebert <paul@cloudfour.com>
2024-04-14 12:49:55 -0700

    Dynamic required fields

--- scripts/rsvp.js
+++ scripts/rsvp.js
@@ -1,9 +1,14 @@
 const comingSelect = document.querySelector('[name="coming"]');
+const requiredIfComing = document.querySelectorAll("[js-required]");
 
 function updateForm() {
   document
     .querySelector("form")
     .classList.toggle("expanded", comingSelect.value === "yes");
+
+  requiredIfComing.forEach((el) => {
+    el.required = comingSelect.value === "yes";
+  });
 }
 
 comingSelect.addEventListener("change", updateForm);
